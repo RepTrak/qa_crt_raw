b6ea9f6a85a941f23e9ee437899797d7aa619852
EC2 Default User <ec2-user@ip-10-12-19-134.eu-west-1.compute.internal>
2023-01-18 22:46:46 +0000

    update

--- utils/main.py
+++ utils/main.py
@@ -1,31 +1,25 @@
-from utils.io.setup import get_logistics
-from utils.io.io import Unzip, transfer_from_s3, resume
-import json
-from utils.io.data_intake_main import get_files_dic, etl
-# from utils.filters.filters_main import filters
-# from utils.awareness.awareness_main import awareness
-# from utils.computations.compute_main import compute
-# from utils.upload.upload_platform_file import send_to_analyzer
-# from utils.computations.imputation import run_imputations
-# from utils.upload.upload_imputation import upload_imp
-# from utils.io.clean_up import clean_up
 import datetime as dt
 from dateutil.relativedelta import relativedelta
-import boto3
+import boto3, os 
 import pandas as pd
 
 class QA_Raw_CRT():
 
-    def __init__(self, execution_year_month_str):
+    def __init__(self):
+        pass
 
-        execution_year_month = dt.datetime.strptime(execution_year_month_str, '%Y-%m').date()
-        self.cur_exe_year = execution_year_month.year
-        self.cur_exe_month = execution_year_month.month
+    def new_path_4_download_file(self, parent_dir, child_dir):
+        # parent_dir = os.getcwd()
+        path = parent_dir + child_dir
+        os.mkdir(path)
+        return path
 
-        previous_execution_year_month = execution_year_month + relativedelta(months=-1)
-        self.pre_exe_year = previous_execution_year_month.year
-        self.pre_exe_month = previous_execution_year_month.month
+    def delete_tmp_folder_file(self, full_dir, filename):
+        os.remove(full_dir+filename)
+        os.rmdir(full_dir)
+        return 
     
+    # def get_json_data_s3(self, year, month, bucketname, parent_dir):
     def get_json_data_s3(self, year, month, bucketname):
 
         s3 = boto3.client('s3')
@@ -38,101 +32,109 @@ class QA_Raw_CRT():
         prefix_objs = bucket.objects.filter(Prefix=subfolder)
 
         dflist = []
+        tmp_path = '/tmp/'
 
         for obj in prefix_objs:
             if obj.key.split('.')[-1] == 'json':
-                fullfilename = "s3://" + bucketname +'/'+ obj.key
-                print(fullfilename)
-                # df_spss = pd.read_spss(fullfilename)
                 dftemp = pd.read_json(obj.get()['Body'])
                 dflist.append(dftemp)
-        df = pd.concat(dflist)
+                del dftemp
+
+            # if obj.key.split('.')[-1] == 'sav':
+          
+            #     filename = obj.key.split('/')[-1]
+            #     full_path = self.new_path_4_download_file(parent_dir, tmp_path)
+            #     bucket.download_file(obj.key, full_path+filename)
+            #     df_spss = pd.read_spss(full_path+filename)
+            #     self.delete_tmp_folder_file(full_path, filename)
+
+        df_json = pd.concat(dflist)
+        del dflist
+
+        # return df_json, df_spss
+        return df_json
+
+    
+    def compare_col(self, cur_cols, lst_cols):
+
+        # new columns for this month
+
+        new_cols = list(set(cur_cols)-set(lst_cols))
+
+        # missing columns for this month
+
+        missing_cols = list(set(lst_cols)-set(cur_cols))
+
+        return new_cols, missing_cols
+    
+    def compare_unique_values_by_col(self, df_cur, df_lst, cols_cur, cols_lst):
+
+        df_unique_value_new, df_unique_value_missing = pd.DataFrame(), pd.DataFrame()
+
+        for col in cols_cur:
+            if col in cols_lst:
+                value_list_cur = df_cur[col].unique().tolist()
+                value_list_lst = df_lst[col].unique().tolist()
+                new_unique_values = self.compare_col(value_list_cur, value_list_lst)
+                missing_unique_values = self.compare_col(value_list_lst, value_list_cur)
+                if len(new_unique_values) != 0:
+                    df_unique_value_new[col] = new_unique_values
+                if len(missing_unique_values) != 0:
+                    df_unique_value_missing[col] = missing_unique_values
+
+        return df_unique_value_new, df_unique_value_missing
+
+    def add_first_column(self, df, new_column_value, new_col_type):
+
+        new_column_name = new_col_type + '_Column_Name'
+
+        df.insert(loc=0, column= new_column_name, value=new_column_value)
 
         return df
     
+    def get_compared_results_from_df(self, df_cur, df_lst):
 
+        cols_cur, cols_lst = df_cur.columns, df_lst.columns
+
+        new_cols, missing_cols = self.compare_col(cols_cur, cols_lst)
+
+        df_new, df_missing = self.compare_unique_values_by_col(df_cur, df_lst, cols_cur, cols_lst)
+
+        df_new = self.add_first_column(df_new, new_cols, new_col_type='New')
+
+        df_missing = self.add_first_column(df_missing, missing_cols, new_col_type='Missing')
+
+        return df_new, df_missing
+    
+    def main(self, execution_year_month_str, bucketname):
+
+        parent_dir = os.getcwd()
+
+        execution_year_month = dt.datetime.strptime(execution_year_month_str, '%Y-%m').date()
+        cur_exe_year = execution_year_month.year
+        cur_exe_month = execution_year_month.month
+
+        previous_execution_year_month = execution_year_month + relativedelta(months=-1)
+        pre_exe_year = previous_execution_year_month.year
+        pre_exe_month = previous_execution_year_month.month
         
         
-            
+        df_cur_json = self.get_json_data_s3(cur_exe_year, cur_exe_month, bucketname)
+
+        df_lst_json = self.get_json_data_s3(pre_exe_year, pre_exe_month, bucketname)
 
 
-    def run(S3_PATH, start, stop, upload, roll):
+        # df_cur_json, df_cur_spss = self.get_json_data_s3(self.cur_exe_year, self.cur_exe_month, bucketname, parent_dir)
 
-        # Organize paths, get important details - always run
-        paths_dic, codebook_df, constructs_df, export_cmd = get_logistics(S3_PATH)
-        ALL_FILES = get_files_dic('./tmp/')
-        if start > 1:
-            df, meta = resume(start, ALL_FILES)
+        # df_lst_json, df_lst_spss = self.get_json_data_s3(self.pre_exe_year, self.pre_exe_month, bucketname, parent_dir)
+        
+        df_new_json, df_missing_json = self.get_compared_results_from_df(df_cur_json, df_lst_json)
 
-        # ---------------------------------------------------
-        # 0: Downloading data
-        if start == 0:
-            print(f"Passing dataset from s3 ({paths_dic['filename_with_path']}) "
-                f"to ec2 (tmp) and unzipping")
-            transfer_from_s3(export_cmd)
-            Unzip(paths_dic['tmp_path'])
-            ALL_FILES = get_files_dic('./tmp/') # Need this here!
-            start +=1
-            if stop:
-                start=8
+        del df_cur_json, df_lst_json
 
-        # ---------------------------------------------------
-        # 1: Reading data
-        if start == 1:
-            json_meta = json.load(open(ALL_FILES['.txt'][0]))
-            # df, meta, SPSS, JSON = etl(ALL_FILES, './tmp/', json_meta)
-            df_json, df_spss = etl(ALL_FILES, './tmp/', json_meta)
-            # df = filters(df)
-            start += 1
-            if stop:
-                start = 8
-        return df_json, df_spss
+        # df_new_spss, df_missing_spss = self.get_compared_results_from_df(df_cur_spss, df_lst_spss)
 
-        # # ---------------------------------------------------
-        # # 2: Awareness
-        # if start == 2:
-        #     awareness(df, ALL_FILES)
-        #     start += 1
-        #     if stop:
-        #         start = 8
+        # del df_cur_spss, df_lst_spss
 
-        # # ---------------------------------------------------
-        # # 3: Computations
-        # if start == 3:
-        #     df = compute(df, codebook_df, constructs_df,
-        #                 upload = upload, roll_cmd = roll)
-        #     start += 1
-        #     if stop:
-        #         start = 8
-
-        # # ---------------------------------------------------
-        # # 4: Upload scores to Platform
-        # if start == 4:
-        #     send_to_analyzer(df)
-        #     start += 1
-        #     if stop:
-        #         start = 8
-
-        # # ---------------------------------------------------
-        # # 5: Imputations
-        # if start == 5:
-        #     df = run_imputations(df, constructs_df)
-        #     start += 1
-        #     if stop:
-        #         start = 8
-
-        # # ---------------------------------------------------
-        # # 6: Upload Imputations
-        # if start == 6:
-        #     zip_filenames = upload_imp(df, upload_cmd=upload,
-        #                             paths_dic=paths_dic, meta=meta)
-        #     start += 1
-        #     if stop:
-        #         start = 8
-
-        # # ---------------------------------------------------
-        # # 7: Clean up!
-        # if start == 7:
-        #     clean_up(zip_filenames, paths_dic)
-
-        # print("\n---\nCRT Processing is complete!\n---")
+        # return df_new_json, df_missing_json, df_new_spss, df_missing_spss  
+        return df_new_json, df_missing_json
